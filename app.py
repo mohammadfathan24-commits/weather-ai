90f39a231c26302dd40ff82bf294cb049eb9a2bc
Fathan <mohammadfathan24@gmail.com>
2026-03-06 20:46:27 +0700

    fix input features

--- app.py
+++ app.py
@@ -7,14 +7,26 @@ model = joblib.load("weather_model.pkl")
 
 st.title("🌦️ AI Prediksi Cuaca")
 
+st.write("Masukkan data cuaca")
+
 temperature = st.number_input("Temperature")
 humidity = st.number_input("Humidity")
 wind_speed = st.number_input("Wind Speed")
+precipitation = st.number_input("Precipitation (%)")
+pressure = st.number_input("Atmospheric Pressure")
+uv_index = st.number_input("UV Index")
+visibility = st.number_input("Visibility (km)")
 
 if st.button("Prediksi Cuaca"):
 
-    data = np.array([[temperature, humidity, wind_speed]])
+    data = np.array([[temperature,
+                      humidity,
+                      wind_speed,
+                      precipitation,
+                      pressure,
+                      uv_index,
+                      visibility]])
 
     prediction = model.predict(data)
 
-    st.success(f"Hasil Prediksi: {prediction}")
+    st.success(f"Hasil Prediksi Cuaca: {prediction[0]}")
